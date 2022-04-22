1f0f4b7d4c08e0e4cdeca5e79d07ef2cdd9a2032
Shinigami <chrissi92@hotmail.de>
2022-04-22 20:42:27 +0200

    Apply suggestions from code review
    Co-authored-by: ST-DDT <ST-DDT@gmx.de>

--- src/faker.ts
+++ src/faker.ts
@@ -171,16 +171,40 @@ export class Faker {
   /**
    * Sets the seed or generates a new one.
    *
-   * Please note that generated values are dependent on both the seed and the number of calls that have been made since it was set.
+   * Please note that generated values are dependent on both the seed and the
+   * number of calls that have been made since it was set.
+   *
+   * This method is intended to allow for consistent values in a tests, so you
+   * might want to use hardcoded values as the seed.
+   *
+   * In addition to that it can be used for creating truly random tests
+   * (by passing no arguments), that still can be reproduced if needed,
+   * by logging the result and explicitly setting it if needed.
    *
    * @param seed The seed to use. Defaults to a random number.
    * @returns The seed that was set.
+   *
+   * @example
+   * // Consistent values for tests:
+   * faker.seed(42)
+   * faker.datatype.number(10); // 4
+   * faker.datatype.number(10); // 8
+   *
+   * faker.seed(42)
+   * faker.datatype.number(10); // 4
+   * faker.datatype.number(10); // 8
+   *
+   * @example
+   * // Random but reproducible tests:
+   * // Simply log the seed, and if you need to reproduce it, insert the seed here
+   * console.log('Running test with seed:', faker.seed());
    */
   seed(seed?: number): number;
   /**
    * Sets the seed or generates a new one.
    *
-   * Please note that generated values are dependent on both the seed and the number of calls that have been made since it was set.
+   * Please note that generated values are dependent on both the seed and the
+   * number of calls that have been made since it was set.
    *
    * @param seedArray The seed array to use.
    * @returns The seed array that was set.
